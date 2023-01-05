11f444ca36c4b2692c4372c172b46e0cdc0b3ec3
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:57:54 +0530

    Update edit-task.component.ts

--- src/app/edit-task/edit-task.component.ts
+++ src/app/edit-task/edit-task.component.ts
@@ -1,48 +1,54 @@
-import { Component, Inject,OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; 
+import { Router, ActivatedRoute } from '@angular/router';
+
 import { ApiService } from '../shared/api.service';
-import { Router } from '@angular/router';
-import {MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
-  styleUrls: ['./edit-task.component.css']
+  styleUrls: ['./edit-task.component.css'],
 })
 export class EdidTaskComponent implements OnInit {
+  taskForm!: FormGroup;
+  id!: number;
+  isAddMode!: boolean;
 
-  taskForm !: FormGroup; 
-  constructor(private formBuilder:FormBuilder, private api:ApiService,private router: Router,@Inject(MAT_DIALOG_DATA) public editData: any){} 
+  constructor(
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.isAddMode = !this.id;
+
     this.taskForm = this.formBuilder.group({
       title: ['', Validators.required],
-      body:['',Validators.required],
-      userId:[Math.floor(100 + Math.random() * 10)]
-    })
-    if (this.editData) {  
-      this.taskForm.controls['title'].setValue(this.editData.title)
-      this.taskForm.controls['body'].setValue(this.editData.body)
+      body: ['', Validators.required],
+      userId: [Math.floor(100 + Math.random() * 10)],
+    });
+
+    if (!this.isAddMode) {
+      this.api.getTask(this.id).subscribe((x) => this.taskForm.patchValue(x));
     }
   }
-  addTask() {   
-    if (!this.editData) {
-      if (this.taskForm.valid) {
-        this.api.postTask(this.taskForm.value)
-          .subscribe({
-            next: (res) => {
-              alert('Product added succesfully')
-              this.taskForm.reset() 
-            },
-            error: () => {
-              alert("Error while adding the product")
-            }
-          })
-      }
-    }  
-    }   
-  clearTask(){
-   this.taskForm.reset()
-  }
 
+  addTask() {
+    if (this.taskForm.valid) {
+      var userid = this.api.postTask(this.taskForm.value).subscribe({
+        next: (res) => {
+          console.log(res, 'res.value');
+          alert('Task Blog Updated succesfully');
+          this.router.navigate(['/']);
+          this.taskForm.reset();
+        },
+        error: () => {
+          alert('Error while Updated the Task Blog');
+        },
+      });
+    }
+  }
 }
