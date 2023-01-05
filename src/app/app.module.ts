e5b99e412091367242b9adda7510fe89d46f35f0
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:52:59 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EdidTaskComponent } from './edit-task/edit-task.component';
-import { DeleteTaskComponent } from './delete-task/delete-task.component';
-import { CreateTaskComponent } from './create-task/create-task.component';
-import { ViewTaskComponent } from './view-task/view-task.component';
+import { BrowserModule } from '@angular/platform-browser'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { DialogpopupComponent } from './shared/dialogpopup/dialogpopup.component';
+import { MatDialogModule } from '@angular/material/dialog'; 
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSortModule } from '@angular/material/sort';
-import {MatTooltipModule} from '@angular/material/tooltip'; 
+import { MatTooltipModule } from '@angular/material/tooltip'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
 
- 
- 
-import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EdidTaskComponent } from './edit-task/edit-task.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { HeaderComponent } from './header/header.component';
+import { DialogpopupComponent } from './shared/dialogpopup/dialogpopup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EdidTaskComponent,
-    DeleteTaskComponent,
     CreateTaskComponent,
     ViewTaskComponent,
     HeaderComponent,
-    DialogpopupComponent, 
+    DialogpopupComponent,
   ],
   imports: [
     BrowserModule,
@@ -51,7 +46,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     ReactiveFormsModule,
     FormsModule,
     MatSortModule,
-    MatTooltipModule,MatFormFieldModule
+    MatTooltipModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
