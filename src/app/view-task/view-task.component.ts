983cfd60f61d8e3093474646da8fa19875670298
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:54:43 +0530

    Update view-task.component.ts

--- src/app/view-task/view-task.component.ts
+++ src/app/view-task/view-task.component.ts
@@ -1,66 +1,65 @@
-import {  Component, ViewChild,OnInit} from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Data } from '@angular/router';
 import { ApiService } from '../shared/api.service';
-import { DialogpopupComponent } from '../shared/dialogpopup/dialogpopup.component';
-import { Router } from '@angular/router';
-import {MatSort} from '@angular/material/sort';
+import { DialogpopupComponent } from '../shared/dialogpopup/dialogpopup.component'; 
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-view-task',
   templateUrl: './view-task.component.html',
-  styleUrls: ['./view-task.component.css']
+  styleUrls: ['./view-task.component.css'],
 })
-export class ViewTaskComponent implements OnInit{
-  displayedColumns: string[] = ['id','userid', 'title', 'body','action'];
-  dataSource = new MatTableDataSource<Data>; 
+export class ViewTaskComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'userid', 'title', 'body', 'action'];
+  dataSource = new MatTableDataSource<Data>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(public dialog: MatDialog,  private api:ApiService  ) { }
-  
-  openDialog(row:any) {
-    this.dialog.open(DialogpopupComponent, {
-      width: '50%',
-      data:row
-    }).afterClosed().subscribe(val=>{
-      if(val == 'save'){
-        this.getallTask();
-      }
-    }); 
-  } 
+  constructor(public dialog: MatDialog, private api: ApiService) {}
+
+  openDialog(row: any) {
+    this.dialog
+      .open(DialogpopupComponent, {
+        width: '50%',
+        data: row,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val == 'save') {
+          this.getallTask();
+        }
+      });
+  }
   ngOnInit() {
-    this.getallTask()
+    this.getallTask();
   }
   getallTask() {
     this.api.getTaskcollection().subscribe({
-      next:(res)=>{
-       this.dataSource = new MatTableDataSource(res)
-       this.dataSource.paginator = this.paginator;
-       this.dataSource.sort = this.sort
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       },
-      error:(err)=>{
-alert("Error while fetching the records!")
-      }
-    })
+      error: (err) => {
+        alert('Error while fetching the records!');
+      },
+    });
   }
 
-  
-  deleteProduct(id:number){
-    this.api.deleteProduct(id)
-    .subscribe({
-      next:(res)=>{
-      alert("Product Deleted Successfully")
-      this.getallTask();
+  deleteProduct(id: number) {
+    this.api.deleteProduct(id).subscribe({
+      next: (res) => {
+        alert('Product Deleted Successfully');
+        this.getallTask();
       },
-      error:(err)=>{
-        alert("error while deleting the product")
-      }
-    })
-  
+      error: (err) => {
+        alert('error while deleting the product');
+      },
+    });
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -70,7 +69,4 @@ alert("Error while fetching the records!")
       this.dataSource.paginator.firstPage();
     }
   }
-  
-  }
-   
-  
+}
