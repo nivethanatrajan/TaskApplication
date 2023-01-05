990e8902f6fd1f735749c85bd134fca993710361
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:56:11 +0530

    Update dialogpopup.component.ts

--- src/app/shared/dialogpopup/dialogpopup.component.ts
+++ src/app/shared/dialogpopup/dialogpopup.component.ts
@@ -1,34 +1,29 @@
-import {Component, Inject} from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ApiService } from '../api.service';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-dialogpopup',
   templateUrl: './dialogpopup.component.html',
-  styleUrls: ['./dialogpopup.component.css']
+  styleUrls: ['./dialogpopup.component.css'],
 })
 export class DialogpopupComponent {
   taskTitle: any;
   taskDescription: any;
-  taskId:number | undefined;
-  taskuserId:number | undefined;
-  
-  
-
-  constructor(private api: ApiService, @Inject(MAT_DIALOG_DATA) public viewData: any) { }
+  taskId!: number;
+  taskuserId!: number;
+  constructor(
+    private api: ApiService,
+    @Inject(MAT_DIALOG_DATA) public viewData: any
+  ) {}
 
   ngOnInit(): void {
-   
-    this.api.getTask(this.viewData.id).subscribe(
-      (data: any) => {
-        this.taskId= data.id;
-        this.taskuserId = data.userId;
-          this.taskTitle = data.title;
-          this.taskDescription = data.body;  
-      })
-      
-     
-
+    this.api.getTask(this.viewData.id).subscribe((data: any) => {
+      this.taskId = data.id;
+      this.taskuserId = data.userId;
+      this.taskTitle = data.title;
+      this.taskDescription = data.body;
+    });
   }
 }
