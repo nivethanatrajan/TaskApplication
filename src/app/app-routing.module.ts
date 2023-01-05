956d97b07c8c0f8b1105f2e6e7226d34715e052b
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:52:24 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'; 
+import { RouterModule, Routes } from '@angular/router';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { EdidTaskComponent } from './edit-task/edit-task.component';
 import { ViewTaskComponent } from './view-task/view-task.component';
 
-const routes: Routes = [  
-
+const routes: Routes = [
   { path: '', component: ViewTaskComponent },
-  { path: 'createtask', component:  CreateTaskComponent },
-  { path: 'edittask', component: EdidTaskComponent },  
-
+  { path: 'createtask', component: CreateTaskComponent },
+  { path: 'edittask/:id', component: EdidTaskComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
