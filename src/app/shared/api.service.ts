1fd34fa7e7fdd714e631a2d9c7043e35a29cf4a1
nivethanatrajan <82083533+nivethanatrajan@users.noreply.github.com>
2023-01-05 22:55:26 +0530

    Update api.service.ts

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,25 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  
   postTask(data: any) {
-    return this.http.post<any>("https://jsonplaceholder.typicode.com/posts",data)
+    return this.http.post<any>(
+      'https://jsonplaceholder.typicode.com/posts',
+      data
+    );
   }
-  getTask(id:number) {
-    return this.http.get<any>("https://jsonplaceholder.typicode.com/posts/"+id)
+  getTask(id: number) {
+    return this.http.get<any>(
+      'https://jsonplaceholder.typicode.com/posts/' + id
+    );
   }
   getTaskcollection() {
-    return this.http.get<any>("https://jsonplaceholder.typicode.com/posts")
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/posts');
   }
-  putTask(data:any, id:number) {
-    return this.http.put<any>("https://jsonplaceholder.typicode.com/posts/"+id,data)
-  } 
-  deleteProduct(id:number){
-    return this.http.delete<any>("https://jsonplaceholder.typicode.com/posts/"+id);
-  } 
+  putTask(data: any, id: number) {
+    return this.http.put<any>(
+      'https://jsonplaceholder.typicode.com/posts/' + id,
+      data
+    );
+  }
+  deleteProduct(id: number) {
+    return this.http.delete<any>(
+      'https://jsonplaceholder.typicode.com/posts/' + id
+    );
+  }
 }
+//("http://localhost:3000/posts",data)
